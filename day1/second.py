a297c730c22a7b34603fc5f145c020bb8904db4e
John Eriksson <john@provideit.se>
2017-12-03 21:58:49 +0100

    day1 challenge 2

---
+++ day1/second.py
@@ -0,0 +1,24 @@
+# Number to test
+test_num = "1212"
+
+# Convert number to list
+test_num_array = list(test_num)
+
+# Initialize sum variable
+sum_of_nums = 0
+
+# Calculate the new offset
+offset = len(test_num_array) / 2
+
+# Go through all of the digits in the list
+for idx, num in enumerate(test_num_array):
+    new_idx = (idx + offset) % len(test_num_array)
+
+    next_num = test_num_array[int(new_idx)]
+
+    # If the two digit matches, add it's value to the sum
+    if next_num == num:
+        sum_of_nums += int(num)
+
+# Print the result
+print(sum_of_nums)
