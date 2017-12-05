94a2b663c1a3c3ea88c866bfa8f6aa2e8bed0ded
John Eriksson <john@provideit.se>
2017-12-05 23:48:15 +0100

    Added comments to :star: 7 and 8

--- day4/first.py
+++ day4/first.py
@@ -1,11 +1,22 @@
+# Read input data from file
 with open("day4/input.txt", "r") as f:
+    # Read lines into list
     lines = f.readlines()
 
+# Create a counter to store the number of correct passphrases
 correct = 0
+# Go through each passphrase
 for line in lines:
+    # Split the phrase into a list of words
     input_arr = line.strip().split(" ")
+    # Get a list of all the unique elements in the word list
     unique_elements = list(set(input_arr))
+
+    # Compare the length of the original word list and
+    # the list of unique elements
     if len(input_arr) == len(unique_elements):
+        # Increment the correct counter if the lengths match (only unique elements)
         correct += 1
 
+# Print the answers
 print(correct)
--- day4/second.py
+++ day4/second.py
@@ -1,21 +1,38 @@
+# Read input data from file
 with open("day4/input.txt", "r") as f:
+    # Read lines into list
     lines = f.readlines()
 
+# Create a counter to store the number of correct passphrases
 correct = len(lines)
+# Go through each passphrase
 for line in lines:
+    # Split the phrase into a list of words
     input_arr = line.strip().split(" ")
 
+    # Go through each word in the phrase
     for (k, word) in enumerate(input_arr):
+        # Split the word into a list of characters
         letters = list(word)
+        # Sort the list
         letters.sort()
+
+        # Go through all the following words in the phrase
         for cmp in input_arr[k+1:]:
+            # Split the word into a list of chars
             cmp_letters = list(cmp)
+            # Sort the list
             cmp_letters.sort()
 
+            # Compare if the original word and the comparison is identical
             if cmp_letters == letters:
+                # Decrement the counter
                 correct -= 1
+                # Break out of the loop
                 break
-        else:
+        else:  # Ugly hack
             continue
         break
+
+# Print the answer
 print(correct)
